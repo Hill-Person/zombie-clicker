28e85c1d3b21926ce0c5c2a06b9fd20e661700e7
Hill-Person <barkerty@gmail.com>
2021-11-03 15:19:31 -0400

    Added weapons and weapon cost with basic html, need multipliers

--- src/js/ZombieKiller.js
+++ src/js/ZombieKiller.js
@@ -1,8 +1,10 @@
 let zombieKillsDisplayElem = document.querySelector('.zombie-kill-counter');
-// find or make a pixel zombie png to click, red drop shadow on click
+// find or make a pixel zombie png to click, red drop shadow on click, or animations nice
 let killZombie= document.querySelector('.kill-zombie');
 let zomebieDefenseSystemCountElem = document.querySelector('.zombie-defense-system-count');
 let addZombieDefenseSystem = document.querySelector('.add-defense-system');
+let weaponsMultiplierCountElem = document.querySelector('.weapon-multiplier');
+let addWeaponsMultiplier = document.querySelector('.add-weapon')
 
 
 let killCount = 0;
@@ -18,15 +20,17 @@ function updateKillCount() {
 }
 
 let zombieDefenseSystemCount = 0
+let defenseSystemCost = 100;
 
 addZombieDefenseSystem.addEventListener("click", () => {
-    if(killCount >= 100) {
-    killCount = killCount-100;
+    if(killCount >= defenseSystemCost) {
+    killCount = killCount-defenseSystemCost;
     zombieDefenseSystemCount++;
     updateDefenseSystemCount();
     updateKillCount();
-    }
     defenseSystem();
+    }
+    
 });
 
 function updateDefenseSystemCount() {
@@ -40,5 +44,32 @@ function updateDefenseSystemCount() {
 function defenseSystem() { killZombie.click(".kill-zombie")
         setInterval("defenseSystem()", 1000);
 }
+
+let weaponsMultiplierCount = 0;
+let weaponCost = 10;
+
+function getWeaponsMultiplierCount() {
+    weaponsMultiplierCountElem.innerHTML = "Weapons owned: " + weaponsMultiplierCount;
+}
+
+function useWeapons() { killZombie.click(".kill-zombie")
+        setInterval("useWeapons()", 1000);
+}
+
+addWeaponsMultiplier.addEventListener("click", ()=> {
+    if (killCount >= weaponCost) {
+        killCount = killCount-weaponCost;
+        weaponsMultiplierCount++;
+        getWeaponsMultiplierCount();
+        updateKillCount();
+    }
+
+});
+
+// probably wrong, need to fix
+// let defenseSystemPercentage = 10;
+// let defenseSystemPercentageCost = (defenseSystemPercentage / 100) * zombieDefenseSystemCount;
+// let defenseSystemCostMultiplier = defenseSystemPercentageCost
+
 
 
