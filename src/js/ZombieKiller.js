72cc4e1f8db309b6140a5c097e3e5cae3b1139ac
Hill-Person <barkerty@gmail.com>
2021-11-04 15:40:31 -0400

    changed and refactored, to do multiplier display then styling

--- src/js/ZombieKiller.js
+++ src/js/ZombieKiller.js
@@ -1,75 +1,12 @@
-let zombieKillsDisplayElem = document.querySelector('.zombie-kill-counter');
-// find or make a pixel zombie png to click, red drop shadow on click, or animations nice
-let killZombie= document.querySelector('.kill-zombie');
-let zomebieDefenseSystemCountElem = document.querySelector('.zombie-defense-system-count');
-let addZombieDefenseSystem = document.querySelector('.add-defense-system');
-let weaponsMultiplierCountElem = document.querySelector('.weapon-multiplier');
-let addWeaponsMultiplier = document.querySelector('.add-weapon')
 
-
-let killCount = 0;
-updateKillCount();
-
-killZombie.addEventListener("click", () => {
-    killCount++;
-    updateKillCount();
-});
-
-function updateKillCount() {
-    zombieKillsDisplayElem.innerHTML = "Zombies Killed: " + killCount;
-}
-
-let zombieDefenseSystemCount = 0
-let defenseSystemCost = 100;
-
-addZombieDefenseSystem.addEventListener("click", () => {
-    if(killCount >= defenseSystemCost) {
-    killCount = killCount-defenseSystemCost;
-    zombieDefenseSystemCount++;
-    updateDefenseSystemCount();
-    updateKillCount();
-    defenseSystem();
+class ZombieKiller {
+    constructor(){
+        this.killCount = 0;
+        this.zombieDefenseSystemCount = 0;
+        this.defenseSystemCost = 100;
+        this.weaponsMultiplierCount = 0;
+        this.weaponCost = 10;
+        this.zombieKillWeaponMultiplier = 1;
+        this.zombieKillDefenseMultiplier = 1;
     }
-    
-});
-
-function updateDefenseSystemCount() {
-    zomebieDefenseSystemCountElem.innerHTML = "Defenses Deployed: " + zombieDefenseSystemCount;
 }
-
-// As kills increase better defenses, trip wire, spike pits, burning pits, then to gun turrets, etc.
-// better weapons pistol, shotgun, assault rifle, machine gun
-// display png pixel weapon when bought?
-
-function defenseSystem() { killZombie.click(".kill-zombie")
-        setInterval("defenseSystem()", 1000);
-}
-
-let weaponsMultiplierCount = 0;
-let weaponCost = 10;
-
-function getWeaponsMultiplierCount() {
-    weaponsMultiplierCountElem.innerHTML = "Weapons owned: " + weaponsMultiplierCount;
-}
-
-function useWeapons() { killZombie.click(".kill-zombie")
-        setInterval("useWeapons()", 1000);
-}
-
-addWeaponsMultiplier.addEventListener("click", ()=> {
-    if (killCount >= weaponCost) {
-        killCount = killCount-weaponCost;
-        weaponsMultiplierCount++;
-        getWeaponsMultiplierCount();
-        updateKillCount();
-    }
-
-});
-
-// probably wrong, need to fix
-// let defenseSystemPercentage = 10;
-// let defenseSystemPercentageCost = (defenseSystemPercentage / 100) * zombieDefenseSystemCount;
-// let defenseSystemCostMultiplier = defenseSystemPercentageCost
-
-
-
---
+++ src/js/app.js
@@ -0,0 +1,86 @@
+let zombieKillsDisplayElem = document.querySelector('.zombie-kill-counter');
+let killZombie = document.querySelector('.kill-zombie');
+let zombieDefenseSystemCountElem = document.querySelector('.zombie-defense-system-count');
+let addZombieDefenseSystem = document.querySelector('.add-defense-system');
+let weaponsMultiplierCountElem = document.querySelector('.weapon-multiplier-count');
+let addWeaponsMultiplier = document.querySelector('.add-weapon');
+const weaponsMultiplerCostElem = document.getElementById('weapons-cost')
+const zombieDefenseSystemCostElem = document.getElementById('defense-system-cost');
+const weaponsMultiplierDisplayElem = document.getElementById('weapons-multiplier');
+const defenseMultiplierDisplayElem = document.getElementById('defenses-multiplier');
+
+
+var zombieKiller = new ZombieKiller();
+var clickInterval = null;
+
+killZombie.addEventListener("click", () => {
+     zombieKiller.killCount += zombieKiller.zombieKillWeaponMultiplier;
+    updateKillCount();
+});
+
+
+function updateKillCount() {
+    zombieKillsDisplayElem.innerHTML = "Zombies Killed: " + zombieKiller.killCount.toFixed(0);
+}
+
+function updateDefenseSystemCount() {
+    zombieDefenseSystemCountElem.innerHTML = "Defenses Deployed: " + zombieKiller.zombieDefenseSystemCount;
+}
+
+function defenseSystem() {
+    if(zombieKiller.zombieDefenseSystemCount >= 1) killZombie.click(".kill-zombie") }
+
+function getWeaponsMultiplierCount() {
+    weaponsMultiplierCountElem.innerHTML = "Weapons owned: " + zombieKiller.weaponsMultiplierCount;
+}
+
+function useWeapons()  {
+if(zombieKiller.weaponsMultiplierCount >= 1) { killZombie.click(".kill-zombie") }
+}
+
+function getWeaponCost() {
+    weaponsMultiplerCostElem.innerText = "Weapon Cost: " + zombieKiller.weaponCost.toFixed(2);
+}
+
+function getDefenseSystemCost() {
+    zombieDefenseSystemCostElem.innerText = "Defense System Cost: " + zombieKiller.defenseSystemCost.toFixed(2);
+}
+
+function getWeaponMultiplier() {
+    weaponsMultiplierDisplayElem.innerText = "Current Weapons Kill Multiplier: " + zombieKiller.zombieKillWeaponMultiplier.toFixed(2)
+}
+
+function getDefenseMultiplier() {
+    defenseMultiplierDisplayElem.innerText = "Current Defenses Kill Multiplier: " + zombieKiller.zombieKillDefenseMultiplier.toFixed(2);
+}
+
+addWeaponsMultiplier.addEventListener("click", ()=> {
+    if (zombieKiller.killCount >= zombieKiller.weaponCost) {
+        zombieKiller.killCount = zombieKiller.killCount-zombieKiller.weaponCost;
+        zombieKiller.weaponsMultiplierCount++;
+        zombieKiller.zombieKillWeaponMultiplier = Math.pow(1.2, zombieKiller.weaponsMultiplierCount);
+        zombieKiller.weaponCost = ((10 / 100) * zombieKiller.weaponsMultiplierCount) + zombieKiller.weaponCost;
+        getWeaponsMultiplierCount();
+        updateKillCount();
+        useWeapons();
+        getWeaponCost();
+    }
+});
+
+addZombieDefenseSystem.addEventListener("click", () => {
+    if(zombieKiller.killCount >= zombieKiller.defenseSystemCost) {
+    zombieKiller.killCount = zombieKiller.killCount-zombieKiller.defenseSystemCost;
+    zombieKiller.zombieDefenseSystemCount++;
+    zombieKiller.zombieKillDefenseMultiplier = Math.pow(1.2, zombieKiller.zombieDefenseSystemCount);
+    zombieKiller.defenseSystemCost = ((10 / 100) * zombieKiller.zombieDefenseSystemCount) + zombieKiller.defenseSystemCost;
+    updateDefenseSystemCount();
+    updateKillCount();
+    defenseSystem();
+    getDefenseSystemCost()
+    }
+    
+});
+
+setInterval("defenseSystem()", 1000);
+
+
